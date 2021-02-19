b922d27f7deee1e1d59b277797b4dda7c78f8279
Jasper Alblas <just_jasper@hotmail.com>
2021-02-19 20:56:51 +0100

    theme files

---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/jasper/Desktop/portfolio/JAlblas.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/jasper/Desktop/portfolio/JAlblas.github.io/src/pages/404.js"))),
+  "component---src-pages-imprint-js": hot(preferDefault(require("/Users/jasper/Desktop/portfolio/JAlblas.github.io/src/pages/imprint.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/jasper/Desktop/portfolio/JAlblas.github.io/src/pages/index.js"))),
+  "component---src-pages-privacy-js": hot(preferDefault(require("/Users/jasper/Desktop/portfolio/JAlblas.github.io/src/pages/privacy.js")))
+}
+
